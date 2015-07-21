98cfa9e6a51b55915f9f9d3ec4ca66907b647cc8
pmat <pmat@BIDAR.softservecom.com>
2015-07-21 13:06:56 +0300

    first page

--- src/main/java/com/test/organizer/controller/PersonController.java
+++ src/main/java/com/test/organizer/controller/PersonController.java
@@ -1,8 +1,14 @@
 package com.test.organizer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PersonController {
 
+	@RequestMapping(value = {"/"})
+	public String registrationPage(){		
+		return "RegistrationPage";
+	}
 }
