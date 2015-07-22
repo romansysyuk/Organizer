a0e06b848aa1e36e2285485f971d0f87d1e41c92
pmat <pmat@BIDAR.softservecom.com>
2015-07-22 15:54:39 +0300

    migrated to java based configuration

---
+++ src/main/java/com/test/organizer/configuration/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.test.organizer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.test.organizer")
+public class ApplicationConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasename("messages");
+//		return messageSource;
+//	}
+}
--- src/main/resources/organizer_bd.sql
+++ src/main/resources/organizer_bd.sql
@@ -11,8 +11,19 @@ create table `person`(
 	`birthday` date not null,
 	`email` varchar(70) not null,
 	`login` varchar(20) not null,
-	`password` varchar(30) not null,
+	`password` varchar(30) not null
 );
+
+insert into `person` values (1,'Petro','Petro','1991-07-10','matyashps@gmail.com','matyashps','matyashgtnhj');
+
+drop table if exists `role`;
+create table `role`(
+	`roleId` int(10) not null auto_increment primary key,
+	`roleName` varchar(20) not null,
+	`personId` int(10) not null
+);
+
+insert into `role` values (1, 'ROLE_ADMIN', 1);
 
 drop table if exists `event`;
 create table `event`(
@@ -21,5 +32,5 @@ create table `event`(
 	`eventName` varchar(50) not null,
 	`personId` int(10) not null,
 	`clasterId` int(10),
-	`description` varchar(1000),
+	`description` varchar(1000)
 );
