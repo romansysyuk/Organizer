47d2896bfa34b37ddf5a5f12fbf39cddaff0e6b4
pmat <pmat@BIDAR.softservecom.com>
2015-07-30 11:42:02 +0300

    qw

--- src/main/java/com/test/organizer/configuration/SpringMvcInitializer.java
+++ src/main/java/com/test/organizer/configuration/SpringMvcInitializer.java
@@ -1,22 +1,22 @@
-package com.test.organizer.configuration;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { AppConfig.class };
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return null;
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
-	}
-	
-}
+//package com.test.organizer.configuration;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//	@Override
+//	protected Class<?>[] getRootConfigClasses() {
+//		return new Class[] { AppConfig.class };
+//	}
+//
+//	@Override
+//	protected Class<?>[] getServletConfigClasses() {
+//		return null;
+//	}
+//
+//	@Override
+//	protected String[] getServletMappings() {
+//		return new String[] { "/" };
+//	}
+//	
+//}
--- src/main/java/com/test/organizer/configuration/SpringSecurityInitializer.java
+++ src/main/java/com/test/organizer/configuration/SpringSecurityInitializer.java
@@ -1,7 +1,7 @@
-package com.test.organizer.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
+//package com.test.organizer.configuration;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+//
+//}
